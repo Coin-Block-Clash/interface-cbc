4959ccff01b52cf5b82d0d1a1977e33d669d4382
ABIN VINOD <abinbvinod@gmail.com>
2024-04-04 00:02:38 +0530

    added connect wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,34 @@
-import Image from "next/image";
+"use client"
+import { useEffect, useState } from 'react';
+import { ConnectKitButton } from 'connectkit';
+import { useAccount } from 'wagmi';
 
 export default function Home() {
+  const [clientLoaded, setClientLoaded] = useState(false);
+  const { address } = useAccount();
+
+
+  useEffect(() => {
+    setClientLoaded(true);
+  }, []);
+
   return (
-   <>
-   hello test main
-   </>
+    <>
+      <div className="flex justify-center flex-col items-center mt-4 gap-4">
+        <h1>TEST WALLET AND DATA INPUT TO DB</h1>
+        <ConnectKitButton />
+        {clientLoaded && (
+          <div className="flex">
+            <h4>Wallet Data</h4>: {address}
+          </div>
+        )}
+        <div className="flex">
+          <h4>User Data</h4> : <input type="text" className="bg-black" placeholder="enter your data here"/>
+        </div>
+        <div className="flex">
+          <h4>User Tag</h4> : <input type="text" className="bg-black" placeholder="enter your game tag here"/>
+        </div>
+      </div>
+    </>
   );
 }
--- src/components/Wallet/WagmiProvider.tsx
+++ src/components/Wallet/WagmiProvider.tsx
@@ -1,14 +1,15 @@
+"use client"
 import { WagmiProvider, createConfig, http } from "wagmi";
-import { mainnet } from "wagmi/chains";
+import { optimismSepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
 const config = createConfig(
   getDefaultConfig({
-    chains: [mainnet],
+    chains: [optimismSepolia],
     transports: {
-      [mainnet.id]: http(
-        `https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+      [optimismSepolia.id]: http(
+        `https://opt-sepolia.g.alchemy.com/v2/VIUNoniLrNB_ahw2ZpQxObfZqp8yVDrT`,
       ),
     },
     walletConnectProjectId: "b3e275bd03d0f994672d0823110b5edf",
